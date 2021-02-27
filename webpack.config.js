99011ab4961f0757e82f2dc50271536e20779a5a
Ludwing <ludwing238@gmail.com>
2021-02-26 18:28:30 -0600

    loader y plugin para css y files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: "./src/start.js",
@@ -6,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     //   Incluyo los loaders para css
     // 1. Utilizo css-loader para transformar a js
@@ -16,13 +18,24 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
           "sass-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            }
+          },
+        ],
+      }
     ],
   },
 };
