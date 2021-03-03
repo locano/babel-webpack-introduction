a9b1db7aa225d295a533ddfbefa616a589ffc3f1
Ludwing <ludwing238@gmail.com>
2021-03-03 14:00:28 -0600

    adding babel-loader a webpack

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,11 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/,
+      },
     ],
   },
 };
