06b1f6c4d35217b0ccdc7f814d817716989489e6
Ludwing <ludwing238@gmail.com>
2021-03-03 14:00:17 -0600

    adding polifyll

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
-const presets = ["@babel/env"];
+const presets = [
+  "@babel/env",
+  {
+    targets: {
+      edge: "17",
+      firefox: "60",
+      chrome: "67",
+      safari: "11.1",
+    },
+    useBuiltIns: "usage", //usage - verifica y utiliza los polifyll correspondientes.
+    corejs: "3.6.5",
+  },
+];
 const plugins = ["@babel/plugin-transform-arrow-functions"];
 
 module.exports = { presets, plugins };
